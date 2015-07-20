26b1c37279d7826a2d27ec63f80e345a61af1a6d
avinav <sharan.avinav@gmail.com>
2015-07-19 20:27:27 -0400

    update server for 'no results'

--- server/app/views.py
+++ server/app/views.py
@@ -8,7 +8,7 @@ from .forms import SearchForm
 def index():
     form = SearchForm()
     query = ""
-    res = []
+    res = None
     if form.validate_on_submit():
         res = queryIndex.query_from_server(form.query.data)
     return render_template("index.html",
